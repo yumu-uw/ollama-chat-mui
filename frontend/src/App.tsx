d8718bd521bf769f380279810675564198475da0
yumuuw <yumuuw@gmail.com>
2025-02-11 12:05:37 +0900

    panda cssの設定を追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,48 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useState } from "react";
+import { styled } from "../styled-system/jsx";
+import { Greet } from "../wailsjs/go/main/App";
+import logo from "./assets/images/logo-universal.png";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+	const [resultText, setResultText] = useState(
+		"Please enter your name below 👇",
+	);
+	const [name, setName] = useState("");
+	const updateName = (e: any) => setName(e.target.value);
+	const updateResultText = (result: string) => setResultText(result);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+	function greet() {
+		Greet(name).then(updateResultText);
+	}
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+	return (
+		<div id="App">
+			<img src={logo} id="logo" alt="logo" />
+			<div id="result" className="result">
+				{resultText}
+			</div>
+			<div id="input" className="input-box">
+				<input
+					id="name"
+					className="input"
+					onChange={updateName}
+					autoComplete="off"
+					name="input"
+					type="text"
+				/>
+				<styled.button
+					bg="blue.500"
+					color="white"
+					py="2"
+					px="4"
+					rounded="md"
+					onClick={greet}
+				>
+					Greet
+				</styled.button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
