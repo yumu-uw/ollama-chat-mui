93db9e07dbe021c38c6a56cf62c6147ca95e7011
yumuuw <yumuuw@gmail.com>
2025-03-11 20:07:20 +0900

    react-routerをインストール

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./css/index.css";
 import App from "./App";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { HashRouter, Route, Routes } from "react-router";
+import { App2 } from "./App2";
+
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<HashRouter>
+			<Routes>
+				<Route index element={<App />} />
+				<Route path="app2" element={<App2 />} />
+			</Routes>
+		</HashRouter>
 	</StrictMode>,
 );
