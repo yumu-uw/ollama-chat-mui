7cad4ee5f4d555b5c28ead07be985965b9c6d6a9
yumuuw <yumuuw@gmail.com>
2025-03-13 22:48:32 +0900

    windowの横幅の最大値を設定

--- main.go
+++ main.go
@@ -32,12 +32,13 @@ func main() {
 
 	// Create application with options
 	err = wails.Run(&options.App{
-		Title:  "ollama-chat",
+		Title:  "Ollama Chat",
 		Width:  1024,
-		Height: 768,
+		Height: 900,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+		MaxWidth:         1200,
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
