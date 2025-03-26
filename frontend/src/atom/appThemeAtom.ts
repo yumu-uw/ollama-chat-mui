ef064945d8e26254ae36a68c44940c2b143173cd
yumuuw <yumuuw@gmail.com>
2025-03-26 10:50:02 +0900

    不要なファイルを削除

--- frontend/src/atom/appThemeAtom.ts
+++
@@ -1,6 +0,0 @@
-import type { AppThemeModel } from "@/model/configModel";
-import { atom } from "jotai";
-
-export const appThemeAtom = atom<AppThemeModel>(
-	document.body.getAttribute("data-theme") as AppThemeModel,
-);
